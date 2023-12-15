de7b84577865373213f6ec1425bb7bf4c0ab28f7
Muhammed Enes ÖZBUNAR <enes.ozbunar@renticar.com>
2023-12-16 02:24:56 +0300

    vite-plugin-vuetify added configure autoimport

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,16 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VuetifyPlugin from 'vite-plugin-vuetify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VuetifyPlugin({
+      autoImport: true,
+      optimizeStyles: true,
+    }),
   ],
   resolve: {
     alias: {
